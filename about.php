c4731622727668471b66f93db14b3551d0ada97d
Eddy123k <144173190+Eddy123k@users.noreply.github.com>
2023-09-05 20:21:36 +0000

    Add files via upload

---
+++ about.php
@@ -0,0 +1,51 @@
+<?php include 'header.php';?>
+
+
+<!-- Content -->
+<div id="content">
+
+    <!-- Intro -->
+    <section class="p-t-b-150">
+        <div class="container">
+            <div class="intro-main">
+                <div class="row">
+
+                    <!-- Intro Detail -->
+                    <div class="col-md-7">
+                        <div class="text-sec padding-right-0">
+                            <h5>Health Check Up At Hospital Management System</h5>
+                            <p>At McGil Hospital, your health and well-being are at the heart of everything we do. We are honored to be your trusted healthcare partner, serving our community with compassion, expertise, and cutting-edge medical care.</p>
+                            <ul class="row">
+                              <li class="col-sm-6">
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                                <p>HMS provide reliable communications and improve public safety areas. </p>
+                              </li>
+                              <li class="col-sm-6">
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                <p>Hopital Management System has licensed and qualified doctors! </p>
+                              </li>
+                              <li class="col-sm-6">
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                                <p>Hospital Management System Supports Online Appointment. Check the appointment tab above. </p>
+                              </li>
+                              <li class="col-sm-6">
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                                <p>HMS qualified doctors offers free counseling to their patients. </p>
+                              </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Intro Timing -->
+                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<!-- Footer -->
+<?php include 'footer.php';?>
